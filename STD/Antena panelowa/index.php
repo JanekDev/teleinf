0d6655efa0064c450fe161606397ba193261e2aa
JanekDev <m.wilinski11@gmail.com>
2021-10-09 12:20:24 +0200

    initial

---
+++ STD/Antena panelowa/index.php
@@ -0,0 +1,55 @@
+<?php
+$url = "https://www.dipol.com.pl/antena_mikropaskowa_panelowa_atk-p1-2_4ghz_5_m_przewodu_wtyk_sma_r-p_8_db_A7130.htm";
+?>
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="1;url=<?php echo $url ?>">
+  <script type="text/javascript">
+    window.location.href = "<?php echo $url ?>"
+  </script>
+  <title>Przekierowanie</title>
+  <style>
+    body {
+      margin: 0;
+      display: grid;
+    }
+
+    #container {
+      display: table;
+      position: absolute;
+      font-family: arial;
+      text-align: center;
+      height: 100%;
+      width: 100%;
+      margin: 0;
+    }
+
+    #center {
+      display: table-cell;
+      vertical-align: middle;
+      margin: 0;
+    }
+
+    #center h1 {
+      margin: 16px;
+    }
+
+    #center p {
+      margin: 16px;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="container">
+    <div id="center">
+      <h1>Trwa przekierowywanie...</h1>
+      <p>Strona znajduje się pod tym adresem: <a href='<?php echo $url ?>'><?php echo $url ?></a></p>
+    </div>
+  </div>
+</body>
+
+</html>
